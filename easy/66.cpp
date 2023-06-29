5d8c56e764f42c2a16a2c1c0aa723366dbbd62ae
Tomas Pereira <tomastorrespereira@gmail.com>
2023-06-30 00:09:40 +0100

    66 exercise-Plus One

---
+++ easy/66.cpp
@@ -0,0 +1,17 @@
+// Exercice 66 PlusOne te-100% spe-70%
+class Solution {
+public:
+	vector<int> plusOne(vector<int>& digits) {
+		for(int i=digits.size()-1;i>=0;i--){
+			if(digits[i]+1<10){
+				digits[i]=digits[i]+1;
+				return digits;
+			}
+			else{
+				digits[i]=0;
+			}
+		}
+		digits.insert(digits.begin(),1);
+		return digits;
+	}
+};
