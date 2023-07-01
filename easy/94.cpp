85ce6ddb66cbafb205ff7859ff385c0580f44a49
Tomas Pereira <tomastorrespereira@gmail.com>
2023-07-01 22:40:59 +0100

    Exercice 94 In order Traversal

---
+++ easy/94.cpp
@@ -0,0 +1,35 @@
+// Exercise In order Traversal of tree- te 23%
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(TreeNode* node,vector<int>& res){
+	if(node->left!=nullptr)
+	    traversal(node->left,res);
+	res.push_back(node->val);
+	if(node->right!=nullptr)
+	    traversal(node->right,res);
+
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+	if(root==nullptr)
+	    return res;
+	if(root->left!=nullptr)
+	    traversal(root->left,res);
+	res.push_back(root->val);
+	if(root->right!=nullptr)
+	    traversal(root->right,res);
+	return res;
+    }
+};
